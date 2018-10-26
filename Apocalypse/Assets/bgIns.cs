92b69f37ca7d4796ad15a60f7c962b0b64166fa3
amr <amritasingh221971@gmail.com>
2018-10-27 03:23:58 +0530

    Forest scene 4th commit
    All assets designed and prefabs created

---
+++ Apocalypse/Assets/bgIns.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bgIns : MonoBehaviour
+{
+    public GameObject g;
+    public GameObject genPnt;
+    private Vector3 gWidth;
+
+    private void Start()
+    {
+        gWidth = g.GetComponent<Renderer>().bounds.size;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < genPnt.transform.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + gWidth.x, transform.position.y, transform.position.z);
+            Instantiate(g, transform.position, transform.rotation);
+
+        }  // Debug.Log("dgfdhg");
+    }
+
+}
