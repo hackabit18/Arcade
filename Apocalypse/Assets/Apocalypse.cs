436a61cfe765d57dcbfb6cd493e192c4f42521bf
anukriti <anukritijha246@gmail.com>
2018-10-27 00:15:41 +0530

    Cover_scene

---
+++ Apocalypse/Assets/Apocalypse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Apocalypse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Chapter()
+    {
+        SceneManager.LoadScene("Chapters_scene");
+    }
+    public void Forest()
+    {
+        SceneManager.LoadScene("Menu_scene");
+
+    }
+
+
+}
