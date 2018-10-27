6aa1513cfd4fd02064ff1211d61aa22adc08afb3
amr <amritasingh221971@gmail.com>
2018-10-27 05:33:19 +0530

    Forest scene  7th commit

---
+++ Apocalypse/Assets/Scripts/collidedByBullet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class collidedByBullet : MonoBehaviour
+{
+
+   
+
+    private void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.CompareTag("bullet"))
+        {
+            Destroy(gameObject,0.2f);
+        }
+    }
+}
---
+++ Apocalypse/Assets/Scripts/killEnemy.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class killEnemy : MonoBehaviour {
+    public Transform player;
+    public SpriteRenderer rb;
+   // private void Start()
+    //{
+        
+    //}
+
+   void FixedUpdate () {
+        float dist = player.position.x - rb.transform.position.x;
+      //Debug.Log("dist = " + dist);
+        if (dist > 0)
+        { rb.flipX = false; }
+        if (dist>-3 && dist<0)
+        {rb.flipX = true;}
+        else
+        { rb.flipX = false; }
+
+        
+    }
+
+   
+}
