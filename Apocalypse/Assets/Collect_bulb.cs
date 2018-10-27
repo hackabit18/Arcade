68ee5b56be84bd873a6019f8a4b959354ee4000b
anukriti <anukritijha246@gmail.com>
2018-10-28 01:35:49 +0530

    City_scene_commit 4

---
+++ Apocalypse/Assets/Collect_bulb.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collect_bulb : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter2D(Collision2D collider)
+    {
+        if(collider.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Bulb collected");
+            Destroy(gameObject);
+        }
+    }
+}
