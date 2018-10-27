6a3e4ee5660f63db928af2e8ec26fb340e8dab4c
guptakj <35427463+guptakj@users.noreply.github.com>
2018-10-27 15:00:49 +0530

    Quit_scene done

---
+++ Apocalypse/Assets/final_seeds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class final_seeds : MonoBehaviour {
+    public Text Final_Score_text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Final_Score_text.text = PlayerPrefs.GetString("finalcount");
+	}
+}
